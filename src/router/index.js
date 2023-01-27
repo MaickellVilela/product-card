c116ddf7469bf5c0b0237d9bdf64ffaced693ff0
Maickell Vilela <maickell@me.com>
2023-01-27 03:24:22 -0300

    routing

--- src/router/index.js
+++ src/router/index.js
@@ -10,18 +10,6 @@ const routes = [
     name: "Home",
     component: Home,
   },
-  {
-    path: "/list",
-    name: "ProductList",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(
-        /* webpackChunkName: "ProductList" */ "../views/ProductList.vue"
-      );
-    },
-  },
 ];
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="">
+    <div class="config">
+      <input
+        id="showRestaurantName"
+        type="checkbox"
+        v-model="showRestaurantName"
+      />
+      <label for="showRestaurantName">Show restaurant name</label>
+    </div>
+    <div class="container">
+      <ProductCard
+        v-for="(product, index) in PRODUCTS"
+        :key="index"
+        :product="product"
+        :showRestaurantName="showRestaurantName"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ProductCard from "@/components/ProductCard.vue";
+import PRODUCTS from "@/mocks/products";
 
 export default {
-  name: "Home",
+  name: "ProductList",
   components: {
-    HelloWorld,
+    ProductCard,
+  },
+  data: function () {
+    return {
+      PRODUCTS,
+      showRestaurantName: false,
+    };
   },
 };
 </script>
+
+<style lang="scss">
+@import "../assets/global-styles.scss";
+
+// ignore this code, this is an example
+.config {
+  display: flex;
+  gap: 0.25rem;
+  padding: px-to-rem(24px);
+}
+
+.container {
+  // contentainer width
+  width: 100%;
+  padding: px-to-rem(8px);
+
+  // centering
+  margin-left: auto;
+  margin-right: auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  @include up-mobile {
+    // contentainer width
+    max-width: 1440px;
+    padding: px-to-rem(32px);
+
+    // grid
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(224px, 1fr));
+    gap: 2rem;
+  }
+}
+</style>
